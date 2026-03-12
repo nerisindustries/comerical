82c7154a01be8cbab1aed967965fea8b5706f138
DFN <daviferreiraneris@gmail.com>
2026-03-12 18:01:01 -0300

    tirei a função action do forms pois não vi necessidade

--- vali.php
+++ vali.php
@@ -8,7 +8,7 @@ date_default_timezone_set("America/Sao_Paulo");
 <html>
 <body>
 
-<form action="vali.php" method="post">
+<form  method="post">
 Usuário: <input type="text" name="name"><br>
 Senha: <input type="password" name="senha"><br>
 <input type="submit">
