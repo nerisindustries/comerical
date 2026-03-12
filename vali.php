23692b17b5cd1680684c9f1cd51b60bcdef949cc
DFN <daviferreiraneris@gmail.com>
2026-03-12 20:22:15 -0300

    Comecei as primeiras partes da validação armazenando as variáveia para futuramente escrevelas na tela, como por exemplo mensagen, ou, usuario

--- vali.php
+++ vali.php
@@ -3,7 +3,7 @@
 
 date_default_timezone_set("America/Sao_Paulo");
 
-
+$mensagem="";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $usuario = $_POST['usuario'];
     $senha = $_POST['senha'];
@@ -13,8 +13,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (strlen($senha) < 4 || strlen($senha) > 15) {
         $mensagem .= "Erro: a senha deve ter entre 4 e 15 caracteres.<br>";
     }
+if($mensagem==""){
+$usuarioValido= ($usuario== "PROFESSOR"||$usuario== "COORDENADOR");
+$senhaValida=($senha=="DEVISATE");
 
 
+}
+}
 ?>
 <html>
 <body>
