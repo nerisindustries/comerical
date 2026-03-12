0b90f4bc9b8d9fa0356d69ad76da3fc9caa13184
DFN <daviferreiraneris@gmail.com>
2026-03-12 19:18:40 -0300

    fazendo a parte de validação de formulários

--- vali.php
+++ vali.php
@@ -1,7 +1,18 @@
 <?php
+
+
 date_default_timezone_set("America/Sao_Paulo");
 
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $usuario = $_POST['usuario'];
+    $senha = $_POST['senha'];
+    if (strlen($usuario) < 4 || strlen($usuario) > 15) {
+        $mensagem .= "Erro: o nome de usuário deve ter entre 4 e 15 caracteres.<br>";
+    }
+    if (strlen($senha) < 4 || strlen($senha) > 15) {
+        $mensagem .= "Erro: a senha deve ter entre 4 e 15 caracteres.<br>";
+    }
 
 
 ?>
@@ -9,7 +20,7 @@ date_default_timezone_set("America/Sao_Paulo");
 <body>
 
 <form  method="post">
-Usuário: <input type="text" name="name"><br>
+Usuário: <input type="text" name="usuario"><br>
 Senha: <input type="password" name="senha"><br>
 <input type="submit">
 </form>
