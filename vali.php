d73a18b03f635602148992398a4b6a783eba5f7e
DFN <daviferreiraneris@gmail.com>
2026-03-12 17:54:05 -0300

    Depois de ter rodado o código e visto um erro que cometi na data, arrumei, agora vou ajeitar o forms

--- vali.php
+++ vali.php
@@ -1,7 +1,18 @@
 <?php
-date_default_timezone_set("Brasil/São_Paulo");
+date_default_timezone_set("America/Sao_Paulo");
 
 
 
 
 ?>
+<html>
+<body>
+
+<form action="vali.php" method="post">
+Usuário: <input type="text" name="name"><br>
+Senha: <input type="password" name="senha"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
