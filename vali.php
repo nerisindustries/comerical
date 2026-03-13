623c6dd400b3e2f39b71ec14415c53efb40dfc8a
DFN <daviferreiraneris@gmail.com>
2026-03-12 23:59:51 -0300

    Terminei a parte de exibição de imagem, onde o programa verifica se mensagem é diferente de vazio, irei revisar o código e ver se há coisas que devo mudar, tive problemas no final, por conta do fechamento de chaves.

--- vali.php
+++ vali.php
@@ -3,39 +3,42 @@
 
 date_default_timezone_set("America/Sao_Paulo");
 
-$mensagem="";
+$mensage="";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $usuario = $_POST['usuario'];
     $senha = $_POST['senha'];
     if (strlen($usuario) < 4 || strlen($usuario) > 15) {
-        $mensagem .= "Nossa, tivemos um erro, o nome de usuário deve ter entre 4 e 15 caracteres.<br>";
+        $mensage .= "Nossa, tivemos um erro, o nome de usuário deve ter entre 4 e 15 caracteres.<br>";
     }
     if (strlen($senha) < 4 || strlen($senha) > 15) {
-        $mensagem .= "Eita, houve um erro, a senha deve ter entre 4 e 15 caracteres.<br>";
+        $mensage .= "Eita, houve um erro, a senha deve ter entre 4 e 15 caracteres.<br>";
     }
-if($mensagem==""){
+if($mensage==""){
 $usuarioValido= ($usuario== "PROFESSOR"||$usuario== "COORDENADOR");
 $senhaValida=($senha=="DEVISATE");
  if($usuarioValido && $senhaValida){
     $hora = date ("H:i");
     $data=date("d/m/Y");
-    $mensagem="Bem-vindo, $usuario! Você realizou acesso às $hora no dia $data.";
-    
-
- }
-
+    $mensage="Seja muito bem-vindo, $usuario! Você realizou acesso às $hora no dia $data.";
+ 
 }elseif(!$usuarioValido && !$senhaValida){
-$mensage="Foi mal ai mano, usuário e senha inválidos, tente novamente"
+$mensage="Foi mal ai mano, usuário e senha inválidos, tente novamente";
 }elseif(!$usuarioValido){
-    $mensage="Ops.....Usuário inválido, tente novamente"
+    $mensage="Ops.....Usuário inválido, tente novamente";
+}elseif(!$senhaValida){
+    $mensage="Opa........ Senha incorreta,tente novamente";
 }
-elseif(!$senhaValida){
-    $mensage="Opa........ Senha incorreta,tente novamente"
 }
 }
+
 ?>
 <html>
 <body>
+    <?php
+    if($mensage!=""){
+        echo"<p><strong>". $mensage."</strong></p>";
+    }
+    ?>
 
 <form  method="post">
 Usuário: <input type="text" name="usuario"><br>
