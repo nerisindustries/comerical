efcc134a297d0295a5aeafebfd6b68a3cba055fb
DFN <daviferreiraneris@gmail.com>
2026-03-12 23:05:32 -0300

    Consegui,terminar a parte de validção, tive um pouco de difculda então, após realizar pesquisas acredito ter compreendido o conteúto pretendo refazer ou fazer outro projeto, agora falta o echo para exibir as mensagens na tela....

--- vali.php
+++ vali.php
@@ -8,16 +8,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $usuario = $_POST['usuario'];
     $senha = $_POST['senha'];
     if (strlen($usuario) < 4 || strlen($usuario) > 15) {
-        $mensagem .= "Erro: o nome de usuário deve ter entre 4 e 15 caracteres.<br>";
+        $mensagem .= "Nossa, tivemos um erro, o nome de usuário deve ter entre 4 e 15 caracteres.<br>";
     }
     if (strlen($senha) < 4 || strlen($senha) > 15) {
-        $mensagem .= "Erro: a senha deve ter entre 4 e 15 caracteres.<br>";
+        $mensagem .= "Eita, houve um erro, a senha deve ter entre 4 e 15 caracteres.<br>";
     }
 if($mensagem==""){
 $usuarioValido= ($usuario== "PROFESSOR"||$usuario== "COORDENADOR");
 $senhaValida=($senha=="DEVISATE");
+ if($usuarioValido && $senhaValida){
+    $hora = date ("H:i");
+    $data=date("d/m/Y");
+    $mensagem="Bem-vindo, $usuario! Você realizou acesso às $hora no dia $data.";
+    
 
+ }
 
+}elseif(!$usuarioValido && !$senhaValida){
+$mensage="Foi mal ai mano, usuário e senha inválidos, tente novamente"
+}elseif(!$usuarioValido){
+    $mensage="Ops.....Usuário inválido, tente novamente"
+}
+elseif(!$senhaValida){
+    $mensage="Opa........ Senha incorreta,tente novamente"
 }
 }
 ?>
